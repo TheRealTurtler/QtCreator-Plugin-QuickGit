cccffad4941fb163c2b18b975e760c7d12a342ba
TheRealTurtler <michael.diedler.7399@gmail.com>
2025-10-21 21:04:02 +0200

    Initial Commit
    - Basic setup for Qt Creator plugin development
    - Added 3 Buttons for testing

---
+++ src/quickgitconstants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace QuickGit::Constants {
+
+const char ACTION_ID[] = "QuickGit.Action";
+const char MENU_ID[] = "QuickGit.Menu";
+
+} // namespace QuickGit::Constants
---
+++ src/quickgittr.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <QCoreApplication>
+
+namespace QuickGit {
+
+struct Tr
+{
+	Q_DECLARE_TR_FUNCTIONS(QtC::QuickGit)
+};
+
+} // namespace QuickGit
